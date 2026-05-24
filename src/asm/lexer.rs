c9d8436330c5ea285020fc27050ae2d6216d1aff
Garry-os <baong2408@gmail.com>
2026-05-24 18:59:23 +0700

    Write a text lexer

---
+++ src/asm/lexer.rs
@@ -0,0 +1,50 @@
+pub enum TokenType {
+    Number,
+    Text,
+    Comma
+}
+
+pub struct Token {
+    pub t_type: TokenType,
+    pub content: String,
+    pub number: i32
+}
+
+pub fn tokenize(src: &String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut buffer = String::new();
+
+    let mut i: usize = 0;
+    let src8 = src.as_bytes();
+    while i < src8.len() {
+        let c = src8[i];
+
+        if c.is_ascii_alphabetic() {
+            // Save to the buffer
+            buffer.push(c as char);
+            i += 1;
+
+            // Keep saving until the end of the token
+            while i < src8.len() && src8[i].is_ascii_alphabetic() {
+                buffer.push(src8[i] as char);
+                i += 1;
+            }
+
+            // Create a new token and push into token list
+            tokens.push(Token {
+                t_type: TokenType::Text,
+                content: buffer.clone(),
+                number: 0
+            });
+
+            buffer.clear();
+        }
+        else if c.is_ascii_whitespace() {
+            // Ignore whitespace
+            i += 1;
+        }
+    }
+
+    tokens
+}
+
--- src/bin/asm.rs
+++ src/bin/asm.rs
@@ -1,3 +1,12 @@
+use arch::asm::lexer::{Token, tokenize};
+
 fn main() {
-    println!("Hello World!");
+    let str = String::from("Hello World from a parser");
+
+    println!("Original string: {}", str);
+
+    let tokens: Vec<Token> = tokenize(&str);
+    for i in 0..tokens.len() {
+        println!("{},", tokens[i].content);
+    }
 }
