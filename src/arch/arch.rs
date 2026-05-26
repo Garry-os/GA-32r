e0313311212fb3652328e04d38bda6e7f0842f10
Garry-os <baong2408@gmail.com>
2026-05-26 16:38:05 +0700

    Build an opcode lookup table

--- src/arch/arch.rs
+++ src/arch/arch.rs
@@ -1,25 +1,29 @@
 // Instructions opcode
-const OP_MOV:   u8 = 0x01;
-const OP_LOAD:  u8 = 0x02;
-const OP_STORE: u8 = 0x03;
-const OP_PUSH:  u8 = 0x04;
-const OP_POP:   u8 = 0x05;
-const OP_JNZ:   u8 = 0x06;
-const OP_ADD:   u8 = 0x07;
-const OP_SUB:   u8 = 0x08;
-const OP_CALL:  u8 = 0x09;
-const OP_RET:   u8 = 0x0A;
-const OP_HLT:   u8 = 0x0B;
-const OP_NOP:   u8 = 0x0C;
-const OP_MOVU:  u8 = 0x0D;
+pub enum Opcode {
+    Mov   = 0x01,
+    Load  = 0x02,
+    Store = 0x03,
+    Push  = 0x04,
+    Pop   = 0x05,
+    Jnz   = 0x06,
+    Add   = 0x07,
+    Sub   = 0x08,
+    Call  = 0x09,
+    Ret   = 0x0A,
+    Hlt   = 0x0B,
+    Nop   = 0x0C,
+    Movu  = 0x0D
+}
 
-// Registers
-const REG_A:    u8 = 0x01;
-const REG_B:    u8 = 0x02;
-const REG_C:    u8 = 0x03;
-const REG_D:    u8 = 0x04;
-const REG_E:    u8 = 0x05;
-const REG_PC:   u8 = 0x06;
-const REG_SP:   u8 = 0x07;
-const REG_IMM:  u8 = 0x08;
+// Registers ID
+pub enum Register {
+    A   = 0x01,
+    B   = 0x02,
+    C   = 0x03,
+    D   = 0x04,
+    E   = 0x05,
+    PC  = 0x06,
+    SP  = 0x07,
+    IMM = 0x08
+}
 
--- src/asm/asm.rs
+++ src/asm/asm.rs
@@ -1,9 +1,86 @@
 use crate::asm::lexer::Token;
+use crate::arch::arch::*;
 
 pub struct Context {
     pub tokens: Vec<Token>,
     pub buffer: Vec<u8>
 }
+
+// Opcode
+pub struct Op {
+    pub name: &'static str,
+    pub num_args: usize,
+    pub opcode: u8
+}
+
+// Opcode lookup table
+pub const OP_TABLE: &[Op] = &[
+    Op {
+        name: "MOV",
+        num_args: 2,
+        opcode: Opcode::Mov as u8
+    },
+    Op {
+        name: "LOAD",
+        num_args: 2,
+        opcode: Opcode::Load as u8
+    },
+    Op {
+        name: "STORE",
+        num_args: 2,
+        opcode: Opcode::Store as u8
+    },
+    Op {
+        name: "PUSH",
+        num_args: 1,
+        opcode: Opcode::Push as u8
+    },
+    Op {
+        name: "POP",
+        num_args: 1,
+        opcode: Opcode::Pop as u8
+    },
+    Op {
+        name: "JNZ",
+        num_args: 1,
+        opcode: Opcode::Jnz as u8
+    },
+    Op {
+        name: "ADD",
+        num_args: 2,
+        opcode: Opcode::Add as u8
+    },
+    Op {
+        name: "SUB",
+        num_args: 2,
+        opcode: Opcode::Sub as u8
+    },
+    Op {
+        name: "CALL",
+        num_args: 1,
+        opcode: Opcode::Call as u8
+    },
+    Op {
+        name: "RET",
+        num_args: 0,
+        opcode: Opcode::Ret as u8
+    },
+    Op {
+        name: "HLT",
+        num_args: 0,
+        opcode: Opcode::Hlt as u8
+    },
+    Op {
+        name: "NOP",
+        num_args: 0,
+        opcode: Opcode::Nop as u8
+    },
+    Op {
+        name: "MOVU",
+        num_args: 2,
+        opcode: Opcode::Movu as u8
+    }
+];
 
 // Should be called when having a compiling error in the source file
 pub fn asm_err(token: &Token, reason: &str) {
