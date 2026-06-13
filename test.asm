21460d951b284fde193f0626f3abcc135d9faa71
garry-os <baong2408@gmail.com>
2026-06-13 10:08:32 +0700

    Setup emulator

--- test.asm
+++ test.asm
@@ -1,5 +1,5 @@
 MOV A, 123 ; Put value 123 into A
 MOV B, 200 ; Put value 200 into B
 ADD A, B ; Add B to A
-LOAD A, 64 ; Load the value 64 into an address stored in A
+STORE A, 64 ; Load the value 64 into an address stored in A
 
